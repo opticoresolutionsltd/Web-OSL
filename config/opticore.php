ad8bdb24086cda49b8b3038ee5e781766801f16b
Rashed Bishal <rashed.bishal@live.com>
2025-02-27 16:03:26 +0600

    set up configuration file for company information

---
+++ config/opticore.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'brand_name' => 'Opticore Solutions Ltd.',
+    'brand_name_absolute' => 'Opticore Solutions Limited',
+    'email_address' => 'osl@opticoresolutionsltd.com',
+    'phone_number' => '+88-01896204400',
+    'social_links' => [
+        'facebook' => 'https://www.facebook.com/opticoresolutionsltd',
+        'youtube' => 'https://www.youtube.com/@OpticoreSolutionsLtd',
+        'behance' => 'https://www.behance.net/opticorsolutionsltd#',
+        'x' => 'https://x.com/osl_opticore',
+        'pinterest' => 'https://www.pinterest.com/opticoresolutionsltd',
+        'instagram' => 'https://www.instagram.com/opticoresolutionsltd',
+        'linkedin' => 'https://www.linkedin.com/in/opticoresolutionsltd',
+    ],
+
+];
