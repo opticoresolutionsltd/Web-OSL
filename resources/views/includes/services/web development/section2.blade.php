bf40fd4ad7042dee952f71bb8f40a97abd3457bf
Rashed Bishal <rashed.bishal@live.com>
2025-02-28 15:48:18 +0600

    add services in web development section

--- resources/views/includes/services/web development/section2.blade.php
+++ resources/views/includes/services/web development/section2.blade.php
@@ -4,10 +4,10 @@
             {{Vite::asset('resources/images/59361.jpg')}}
         </x-slot>
         <x-slot name="heading">
-            Service One
+            Portfolio Website
         </x-slot>
         <x-slot name="description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio.
+            Attract more clients with a professionally designed portfolio website that showcases your best work and elevates your brand. We create visually stunning, SEO-optimized portfolios to help you stand out and win more opportunities.
         </x-slot>
     </x-services.sectionLToR>
 
@@ -16,10 +16,10 @@
             {{Vite::asset('resources/images/4280794.jpg')}}
         </x-slot>
         <x-slot name="heading">
-            Service Two
+            Business Website
         </x-slot>
         <x-slot name="description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio.
+            Boost engagement and drive sales with a business website that resonates with your audience. Our expertly crafted websites focus on user experience, clear communication of your brand’s value, and delivering measurable results.
         </x-slot>
     </x-services.sectionRToL>
 
@@ -28,10 +28,22 @@
             {{Vite::asset('resources/images/59361.jpg')}}
         </x-slot>
         <x-slot name="heading">
-            Service Three
+            Corporate Website
         </x-slot>
         <x-slot name="description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio.
+            Build trust and credibility with a modern corporate website that clearly communicates your mission, values, and brand identity. We design professional, stakeholder-focused sites that reinforce your company’s reputation and long-term success.
         </x-slot>
     </x-services.sectionLToR>
+
+    <x-services.sectionRToL>
+        <x-slot name="imageSrc">
+            {{Vite::asset('resources/images/4280794.jpg')}}
+        </x-slot>
+        <x-slot name="heading">
+            E-commerce Website
+        </x-slot>
+        <x-slot name="description">
+            Maximize conversions and customer satisfaction with an e-commerce site designed for seamless navigation and high performance. Our optimized platforms reduce cart abandonment, enhance user experience, and drive sales for your business.
+        </x-slot>
+    </x-services.sectionRToL>
 </section>
