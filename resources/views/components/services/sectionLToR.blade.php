01e48465d5700841c4c1a9fc0ce66a7286508c86
Rashed Bishal <rashed.bishal@live.com>
2025-03-05 12:33:23 +0600

    Each categories of each services, header color change

--- resources/views/components/services/sectionLToR.blade.php
+++ resources/views/components/services/sectionLToR.blade.php
@@ -1,7 +1,9 @@
 <div class="flex flex-col sm:flex-row justify-center items-center my-8 sm:px-4 md:px-8 lg:px-12 xl:px-28 2xl:px-36">
     <x-images.image src="{{$imageSrc}}" alt="Service 1" class="hidden sm:block sm:w-2/6 md:w-1/3 object-cover mb-4 md:mb-0 animate-fade-in-from-left"/>
     <div class="p-4 animate-fade-in-from-right">
-        <h2 class="text-2xl font-bold mb-2">{{$heading}}</h2>
+        <h2 class="text-2xl text-[#1B4071] font-bold mb-2">{{$heading}}</h2>
         <p class="text-lg text-justify">{{$description}}</p>
     </div>
 </div>
+
+
--- resources/views/components/services/sectionRToL.blade.php
+++ resources/views/components/services/sectionRToL.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col sm:flex-row justify-center items-center my-8 bg-gray-150 sm:px-4 md:px-8 lg:px-12 xl:px-28 2xl:px-36">
     <div class="p-4 sm:py-4 sm:pr-4 sm:pl-0 animate-fade-in-from-left">
-        <h2 class="text-2xl font-bold mb-2">{{$heading}}</h2>
+        <h2 class="text-2xl text-[#1B4071] font-bold mb-2">{{$heading}}</h2>
         <p class="text-lg text-justify">{{$description}}</p>
     </div>
     <x-images.image src="{{$imageSrc}}" alt="Service 2" class="hidden sm:block sm:w-2/6 md:w-1/3 object-cover mb-4 md:mb-0 animate-fade-in-from-right" />
