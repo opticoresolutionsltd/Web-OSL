0b4c94d5b6dd48f079116562f905091667db06ee
Rashed Bishal <rashed.bishal@live.com>
2025-03-05 16:48:14 +0600

    messenger and whatsapp links have been activated

--- config/opticore.php
+++ config/opticore.php
@@ -13,6 +13,8 @@ return [
         'pinterest' => 'https://www.pinterest.com/opticoresolutionsltd',
         'instagram' => 'https://www.instagram.com/opticoresolutionsltd',
         'linkedin' => 'https://www.linkedin.com/in/opticoresolutionsltd',
+        'messenger' => 'https://m.me/opticoresolutionsltd',
+        'whatsapp' => 'https://wa.me/8801896204400',
     ],
     'google_map' => [
         'embed' => 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.563599334598!2d90.36871817602399!3d23.798549686924808!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c174ca2f7d33%3A0x19970f5da700b453!2sOpticore%20Solutions%20Ltd!5e0!3m2!1sen!2sbd!4v1740722159258!5m2!1sen!2sbd',
