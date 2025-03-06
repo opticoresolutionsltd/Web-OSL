a2aa4aefaf84175b5b8002d4a0691489d5403e2c
Rashed Bishal <rashed.bishal@live.com>
2025-03-06 13:21:44 +0600

    update required meta information for graphic design page

--- resources/views/main/services/graphic-design.blade.php
+++ resources/views/main/services/graphic-design.blade.php
@@ -3,11 +3,11 @@
     <x-titles.title>Graphic Design - {{config('opticore.brand_name_absolute')}}</x-titles.title>
 @endsection
 @section('meta-tags')
-    <meta name="description" content="{{config('opticore.brand_name')}}">
-    <meta name="keywords" content="key1, key2, key3">
+    <meta name="description" content="Creative graphic design services, including logo design, branding, social media graphics, and marketing materials.">
+    <meta name="keywords" content="graphic design, branding, logo design, social media graphics, business card design, marketing materials">
     <link rel="canonical" href="{{config('opticore.url')}}graphic-design">
     <meta property="og:title" content="Graphic Design - {{config('opticore.brand_name')}}">
-    <meta property="og:description" content="Affordable Services for All">
+    <meta property="og:description" content="Need a stunning design? Opticore Solution Ltd. provides professional graphic design services for all business needs.">
     <meta property="og:url" content="{{config('opticore.url')}}graphic-design">
     <meta property="og:locale" content="en_US">
     <meta property="og:image" content="">
@@ -15,7 +15,7 @@
     <meta property="og:site_name" content="{{config('opticore.brand_name')}}">
     <meta name="twitter:card" content="summery_large_image">
     <meta name="twitter:title" content="Graphic Design - {{config('opticore.brand_name')}}">
-    <meta name="twitter:description" content="Affordable Services for All">
+    <meta name="twitter:description" content="Get top-notch graphic design services, from logos to branding materials. Enhance your brand identity with our expertise.">
     <meta name="twitter:site" content="@osl_opticore">
     <meta name="twitter:image" content="">
     <meta name="twitter:image:alt" content="{{config('opticore.brand_name')}}">
