1dd4ee5655203cb3fa85720e412364e8f56a48d5
Rashed Bishal <rashed.bishal@live.com>
2025-03-06 13:26:16 +0600

    update required meta information for about us page

--- resources/views/main/others/about.blade.php
+++ resources/views/main/others/about.blade.php
@@ -3,11 +3,11 @@
     <x-titles.title>About Us - {{config('opticore.brand_name_absolute')}}</x-titles.title>
 @endsection
 @section('meta-tags')
-    <meta name="description" content="{{config('opticore.brand_name')}}">
-    <meta name="keywords" content="key1, key2, key3">
+    <meta name="description" content="Learn more about Opticore Solution Ltd., our mission, and our expertise in digital services.">
+    <meta name="keywords" content="about Opticore Solution Ltd., company history, digital agency, who we are">
     <link rel="canonical" href="{{config('opticore.url')}}about-us">
     <meta property="og:title" content="About Us - {{config('opticore.brand_name')}}">
-    <meta property="og:description" content="Affordable Services for All">
+    <meta property="og:description" content="Discover Opticore Solution Ltd.'s journey, mission, and expertise in digital marketing and creative solutions.">
     <meta property="og:url" content="{{config('opticore.url')}}about-us">
     <meta property="og:locale" content="en_US">
     <meta property="og:image" content="">
@@ -15,7 +15,7 @@
     <meta property="og:site_name" content="{{config('opticore.brand_name')}}">
     <meta name="twitter:card" content="summery_large_image">
     <meta name="twitter:title" content="About Us - {{config('opticore.brand_name')}}">
-    <meta name="twitter:description" content="Affordable Services for All">
+    <meta name="twitter:description" content="Meet the team behind Opticore Solution Ltd. and explore our passion for digital excellence.">
     <meta name="twitter:site" content="@osl_opticore">
     <meta name="twitter:image" content="">
     <meta name="twitter:image:alt" content="{{config('opticore.brand_name')}}">
