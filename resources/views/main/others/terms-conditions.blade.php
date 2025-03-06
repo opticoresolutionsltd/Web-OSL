9dd2497655b6df438ce62fb6a5fa65309c05e890
Rashed Bishal <rashed.bishal@live.com>
2025-03-06 13:27:29 +0600

    update required meta information for terms-conditions page

--- resources/views/main/others/terms-conditions.blade.php
+++ resources/views/main/others/terms-conditions.blade.php
@@ -3,11 +3,11 @@
     <x-titles.title>Terms & Conditions - {{config('opticore.brand_name_absolute')}}</x-titles.title>
 @endsection
 @section('meta-tags')
-    <meta name="description" content="{{config('opticore.brand_name')}}">
-    <meta name="keywords" content="key1, key2, key3">
+    <meta name="description" content="Read our terms and conditions to understand our service policies and legal guidelines.">
+    <meta name="keywords" content="terms and conditions, Opticore Solution Ltd. policies, service terms">
     <link rel="canonical" href="{{config('opticore.url')}}terms-conditions">
     <meta property="og:title" content="Terms & Conditions - {{config('opticore.brand_name')}}">
-    <meta property="og:description" content="Affordable Services for All">
+    <meta property="og:description" content="Familiarize yourself with our terms and conditions before using our services.">
     <meta property="og:url" content="{{config('opticore.url')}}terms-conditions">
     <meta property="og:locale" content="en_US">
     <meta property="og:image" content="">
@@ -15,7 +15,7 @@
     <meta property="og:site_name" content="{{config('opticore.brand_name')}}">
     <meta name="twitter:card" content="summery_large_image">
     <meta name="twitter:title" content="Terms & Conditions - {{config('opticore.brand_name')}}">
-    <meta name="twitter:description" content="Affordable Services for All">
+    <meta name="twitter:description" content="Learn about the terms and conditions governing our services.">
     <meta name="twitter:site" content="@osl_opticore">
     <meta name="twitter:image" content="">
     <meta name="twitter:image:alt" content="{{config('opticore.brand_name')}}">
