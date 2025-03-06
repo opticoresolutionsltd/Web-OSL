0d6e0245d44658809eba541526a51f5b67853feb
Rashed Bishal <rashed.bishal@live.com>
2025-03-06 12:53:55 +0600

    update required meta information for faq page

--- resources/views/main/others/faq.blade.php
+++ resources/views/main/others/faq.blade.php
@@ -3,11 +3,11 @@
     <x-titles.title>FAQs' - {{config('opticore.brand_name_absolute')}}</x-titles.title>
 @endsection
 @section('meta-tags')
-    <meta name="description" content="{{config('opticore.brand_name')}}">
-    <meta name="keywords" content="key1, key2, key3">
+    <meta name="description" content="Find answers to commonly asked questions about our services, pricing, and work process.">
+    <meta name="keywords" content="FAQ, Opticore Solution Ltd. questions, digital services FAQ, customer support">
     <link rel="canonical" href="{{config('opticore.url')}}faq">
     <meta property="og:title" content="FAQs' - {{config('opticore.brand_name')}}">
-    <meta property="og:description" content="Affordable Services for All">
+    <meta property="og:description" content="Got questions? Check our FAQ section for details on our services, pricing, and more.">
     <meta property="og:url" content="{{config('opticore.url')}}faq">
     <meta property="og:locale" content="en_US">
     <meta property="og:image" content="">
@@ -15,7 +15,7 @@
     <meta property="og:site_name" content="{{config('opticore.brand_name')}}.">
     <meta name="twitter:card" content="summery_large_image">
     <meta name="twitter:title" content="FAQs' - {{config('opticore.brand_name')}}">
-    <meta name="twitter:description" content="Affordable Services for All">
+    <meta name="twitter:description" content="Learn more about our services with our comprehensive FAQ section.">
     <meta name="twitter:site" content="@osl_opticore">
     <meta name="twitter:image" content="">
     <meta name="twitter:image:alt" content="{{config('opticore.brand_name')}}">
