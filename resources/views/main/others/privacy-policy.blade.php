ad6bde3101db0e4829ecb7e596f916aff0c6481d
Rashed Bishal <rashed.bishal@live.com>
2025-03-06 13:29:14 +0600

    update required meta information for privacy-policy page

--- resources/views/main/others/privacy-policy.blade.php
+++ resources/views/main/others/privacy-policy.blade.php
@@ -3,11 +3,11 @@
     <x-titles.title>Privacy Policy - {{config('opticore.brand_name_absolute')}}</x-titles.title>
 @endsection
 @section('meta-tags')
-    <meta name="description" content="{{config('opticore.brand_name')}}">
-    <meta name="keywords" content="key1, key2, key3">
+    <meta name="description" content="Read our privacy policy to understand how we handle your personal data and ensure security.">
+    <meta name="keywords" content="privacy policy, data protection, Opticore Solution Ltd. privacy, user data security">
     <link rel="canonical" href="{{config('opticore.url')}}privacy-policy">
     <meta property="og:title" content="Privacy & Policy - {{config('opticore.brand_name')}}">
-    <meta property="og:description" content="Affordable Services for All">
+    <meta property="og:description" content="Your privacy matters to us. Read our privacy policy for details on data protection and security.">
     <meta property="og:url" content="{{config('opticore.url')}}privacy-policy">
     <meta property="og:locale" content="en_US">
     <meta property="og:image" content="">
@@ -15,7 +15,7 @@
     <meta property="og:site_name" content="{{config('opticore.brand_name')}}">
     <meta name="twitter:card" content="summery_large_image">
     <meta name="twitter:title" content="Privacy Policy - {{config('opticore.brand_name')}}">
-    <meta name="twitter:description" content="Affordable Services for All">
+    <meta name="twitter:description" content="Find out how we protect your privacy and manage your data securely.">
     <meta name="twitter:site" content="@osl_opticore">
     <meta name="twitter:image" content="">
     <meta name="twitter:image:alt" content="{{config('opticore.brand_name')}}">
