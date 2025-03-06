a4b5d7c714a23f8ca3ed288c0e50a63f5295f4bc
Rashed Bishal <rashed.bishal@live.com>
2025-03-06 13:24:34 +0600

    update requied meta information for 3d production page

--- resources/views/main/services/3d-production.blade.php
+++ resources/views/main/services/3d-production.blade.php
@@ -3,11 +3,11 @@
     <x-titles.title>3D Modeling - {{config('opticore.brand_name_absolute')}}</x-titles.title>
 @endsection
 @section('meta-tags')
-    <meta name="description" content="{{config('opticore.brand_name')}}">
-    <meta name="keywords" content="key1, key2, key3">
+    <meta name="description" content="High-quality 3D production services, including modeling, animation, and architectural visualization.">
+    <meta name="keywords" content="3D modeling, 3D animation, architectural visualization, product rendering, 3D printing">
     <link rel="canonical" href="{{config('opticore.url')}}3d-production">
     <meta property="og:title" content="3D Modeling - {{config('opticore.brand_name')}}">
-    <meta property="og:description" content="Affordable Services for All">
+    <meta property="og:description" content="Bring your ideas to life with our professional 3D modeling and animation services.">
     <meta property="og:url" content="{{config('opticore.url')}}3d-production">
     <meta property="og:locale" content="en_US">
     <meta property="og:image" content="">
@@ -15,7 +15,7 @@
     <meta property="og:site_name" content="{{config('opticore.brand_name')}}">
     <meta name="twitter:card" content="summery_large_image">
     <meta name="twitter:title" content="3D Modeling - {{config('opticore.brand_name')}}">
-    <meta name="twitter:description" content="Affordable Services for All">
+    <meta name="twitter:description" content="Need 3D visuals? Opticore Solution Ltd. offers professional 3D production services for various industries.">
     <meta name="twitter:site" content="@osl_opticore">
     <meta name="twitter:image" content="">
     <meta name="twitter:image:alt" content="{{config('opticore.brand_name')}}">
